e4936e2f43e672718727ca94bf65e97f36053d0d
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-09-03 21:30:08 +0530

    Update password.py

--- password.py
+++ password.py
@@ -7,7 +7,7 @@ ran1=string.ascii_uppercase
 ran2=string.ascii_lowercase
 ran3=string.punctuation
 ran4=string.digits
-print("Tools to generate Random Password using Termux")
+print("Tools to generate Random Password using Termux\n By Mukesh ")
 print(" Hint ? Password Strength \n [1-test , 2-lowest , 3-lower ]\n[ 4- low 5-Normal , 6-Strong] \n[ 7- strongest, 8-very-Strong]\n")
 password=int(input('Enter Password length\n'))
 ran=[]
